eeeaf702c623a25e2e8753722b0bdbae491f9cb0
Romylson <romylsonsousa@hotmail.com>
2017-10-10 11:06:21 -0300

    removendo comentários

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,4 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+DATABASES['default'].update(db_from_env)
 STATIC_URL = '/static/'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PHOTO','https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
