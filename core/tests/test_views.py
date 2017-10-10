2690da30edc87639dc94ac0e5b5fa273e6ea55a5
Romylson <romylsonsousa@hotmail.com>
2017-10-10 20:58:20 -0300

    atualizando

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,6 +1,6 @@
 #coding:utf-8
-from django.test import TestCase,Client
-from django.core.urlresolvers import  reverse
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
 class IndexViewTestCase(TestCase):
 
