23fef021f9eb40bd419d69bb9a26891a4215ad89
Romylson <romylsonsousa@hotmail.com>
2017-10-11 14:57:39 -0300

    atualizando

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -117,7 +117,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
@@ -125,7 +125,7 @@ DATABASES['default'].update(db_from_env)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = os.path.join((BASE_DIR), 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
