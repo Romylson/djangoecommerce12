9b6ef7a653eb025caa91a49b20a1f08505b99b11
Romylson <romylsonsousa@hotmail.com>
2017-10-11 17:40:53 -0300

    atualizando

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'djangoecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': ['/djangoecommercer/core/Templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
