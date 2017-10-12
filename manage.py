2e3730a1661b5d481c95ab657d5009e456dd5ab3
Romylson <romylsonsousa@hotmail.com>
2017-10-12 19:26:47 -0300

    atualizando

--- manage.py
+++ manage.py
@@ -4,7 +4,8 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoecommerce.settings")
+
         from django.core.management import execute_from_command_line
-           import django
-    execute_from_command_line(sys.argv)
+
+      execute_from_command_line(sys.argv)
 
