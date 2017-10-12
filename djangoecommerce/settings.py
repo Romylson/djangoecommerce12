325e658b29aceff1780b919b922c1d20ef36a86c
Romylson <romylsonsousa@hotmail.com>
2017-10-12 18:56:25 -0300

    ajustando o README

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '=vp4kkwpni2ej5p!$ddu(u$-g1rmm)ol3t2u8zoe68t+r!_c8$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -129,3 +129,7 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
+try:
+    from.local_settings import *
+except ImportError:
+    pass
