3b24c066f8ba7b1cdd0e698e84de877cde40e70b
Romylson <romylsonsousa@hotmail.com>
2017-10-12 19:25:15 -0300

    atualizando

--- manage.py
+++ manage.py
@@ -4,17 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoecommerce.settings")
-    try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-            )
-        raise
+           import django
     execute_from_command_line(sys.argv)
 
