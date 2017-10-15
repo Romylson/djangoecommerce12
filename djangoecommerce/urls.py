9c40d030c4b36fca404b53ddcad7b8ca9bf6996f
Romylson <romylsonsousa@hotmail.com>
2017-10-15 04:34:20 -0200

    atualizando

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-# coding:utf-8
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve as serve_static
 
 from core import views
 from catalog import views as views_catalog
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
